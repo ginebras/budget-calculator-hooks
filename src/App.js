1da655461344c54d9a85da2512697c72a975430a
ginebras <alenicofranco@gmail.com>
2021-12-25 21:38:35 -0300

    1 file modified

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './css/app.css';
 import { v4 as uuid } from 'uuid';
 
@@ -7,11 +7,13 @@ import ExpenseForm from './components/ExpenseForm';
 import ExpenseList from './components/ExpenseList';
 import Alert from './components/Alert';
 
-const initialExpenses = [
+/*const initialExpenses = [
   { id: uuid(), charge: 'car payment', amount: 1233 },
   { id: uuid(), charge: 'water', amount: 163 },
   { id: uuid(), charge: 'department', amount: 2000 },
-];
+];*/
+
+var initialExpenses = [];
 
 function App() {
   const [expenses, setExpenses] = useState(initialExpenses);
@@ -23,6 +25,16 @@ function App() {
 
   const [edit, setEdit] = useState(false);
   const [id, setId] = useState(0);
+
+  useEffect(() => {
+    console.log('useEffect ejecutado');
+
+    initialExpenses = localStorage.getItem('expenses')
+      ? JSON.parse(localStorage.getItem('expenses'))
+      : [];
+
+    localStorage.setItem('expenses', JSON.stringify(expenses));
+  }, [expenses]);
 
   const handleCharge = (e) => {
     setCharge(e.target.value);
@@ -41,25 +53,38 @@ function App() {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log(charge, amount);
 
     if (charge !== '' && amount > 0) {
-      var singleExpense = { id: uuid(), charge, amount };
-      setExpenses([...expenses, singleExpense]);
+      if (edit) {
+        let tempExpenses = expenses.map((item) => {
+          return item.id === id ? { ...item, charge, amount } : item;
+        });
+        setExpenses(tempExpenses);
+        setEdit(false);
+        handleAlert({ type: 'success', msg: 'Se ha editado un gasto' });
+      } else {
+        var singleExpense = { id: uuid(), charge, amount };
+        setExpenses([...expenses, singleExpense]);
+        handleAlert({
+          type: 'success',
+          msg: 'Se ha añadido el gasto a la lista',
+        });
+      }
+
       setCharge('');
       setAmount('');
-      handleAlert({
-        type: 'success',
-        msg: 'Se ha añadido el gasto a la lista',
-      });
+      setEdit(false);
     } else {
       handleAlert({ type: 'danger', msg: 'Los campos están vacios' });
     }
   };
 
   const handleClear = () => {
-    console.log('Clear');
-    setExpenses([]);
+    if (!expenses) {
+      handleAlert({ type: 'danger', msg: 'No hay gastos qué eliminar' });
+    } else {
+      setExpenses([]);
+    }
     handleAlert({ type: 'success', msg: 'Se han eliminado todos los gastos' });
   };
 
